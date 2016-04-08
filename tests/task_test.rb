42edc061889961aa0d08dfe489ac51621a26ba48
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-08 11:22:44 -0400

    Edit/add more tests

--- tests/task_test.rb
+++ tests/task_test.rb
@@ -4,7 +4,7 @@ require_relative '../app/models/task'
 class TaskTest < Minitest::Test
 
   def setup
-    @task = Task.create(task: "Do the thing", completed: false)
+    @task = Task.create(task: "Do the thing", completed: false, due_date: Time.new(2016, 1, 1))
     @user = User.create(email: "bob@email.com")
   end
 
@@ -13,21 +13,45 @@ class TaskTest < Minitest::Test
     @user.destroy
   end
 
+  # 1
   def test_task_class_exists
     assert @task
   end
 
+  # 2
   def test_task_has_description
     assert_equal "Do the thing", @task.task
   end
 
-  def task_has_a_status
+  # 3
+  def test_task_has_a_status
     assert_equal false, @task.completed
   end
 
+  # 4
+  def test_task_status_can_be_changed
+    @task.completed = true
+
+    assert_equal true, @task.completed
+  end
+
+  # 5
+  def test_task_has_a_due_date
+    assert_equal Time.new(2016, 1, 1), @task.due_date
+  end
+
+  # 6
+  def test_task_due_date_can_be_changed
+    @task.due_date = Time.new(2016, 2, 2)
+
+    assert_equal Time.new(2016, 2, 2), @task.due_date
+  end
+
+  # 7
   def test_task_belongs_to_user
     @user.tasks << @task
 
     assert_equal true, @task.user_id?
   end
+  
 end
--- tests/user_test.rb
+++ tests/user_test.rb
@@ -5,8 +5,8 @@ class UserTest < Minitest::Test
 
   def setup
     @user = User.create(email: "bob@email.com")
-    @task_1 = Task.create(task: "First task", completed: false)
-    @task_2 = Task.create(task: "Second task", completed: false)
+    @task_1 = Task.create(task: "First task", completed: false, due_date: Time.new(2016, 2, 2))
+    @task_2 = Task.create(task: "Second task", completed: false, due_date: Time.new(2016, 3, 3))
   end
 
   def teardown
@@ -15,18 +15,22 @@ class UserTest < Minitest::Test
     @task_2.destroy
   end
 
+  # 1
   def test_user_class_exists
     assert @user
   end
 
+  # 2
   def test_user_has_email
     assert_equal "bob@email.com", @user.email
   end
 
+  # 3
   def test_user_can_have_many_tasks
     @user.tasks << @task_1
     @user.tasks << @task_2
 
     assert_equal [@task_1, @task_2], @user.tasks
   end
+  
 end
